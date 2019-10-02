c13206a95959628b338b8732555a4880e3590648
venufs <venufs@gmail.com>
2019-10-02 12:01:06 +0530

    chg

--- src/main/java/org/tharak/core/service/DotEndpoint.java
+++ src/main/java/org/tharak/core/service/DotEndpoint.java
@@ -7,7 +7,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Timestamp;
 import java.sql.Types;
 import java.util.ArrayList;
 import java.util.Date;
